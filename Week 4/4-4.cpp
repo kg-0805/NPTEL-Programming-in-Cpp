fb3d6be9bc107b16142d55578739a16af4eb72e1
Kartik Gupta <43263442+kg-0805@users.noreply.github.com>
2020-04-18 18:36:54 +0530

    Create 4-4.cpp

---
+++ Week 4/4-4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class myClass {
+int data_;
+public:
+myClass(int d) : data_(d) { }
+void print() { cout << data_; }
+myClass operator+(myClass& m) {
+myClass m1(data_);
+m1.data_ += m.data_;
+return m1;
+}
+myClass operator-(myClass& m) {
+myClass m1(m.data
+); // LINE-1
+m1.data -= data ; // LINE-2
+return m1;
+}
+};
+int main() {
+int i, j, k;
+cin >> i >> j >> k;
+myClass A(i), B(j), C(k);
+myClass D = A + B - C;
+D.print();
+return 0;
+}
